032e4d59a5e694135d63946ee4456766c3304047
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2022-11-30 18:54:15 +0000

    updated error handling message response

--- controllers/books.js
+++ controllers/books.js
@@ -7,11 +7,12 @@ const BookSeeder = require('../seeders/books_seeder');
 books.get('/', (req, res) => {
     db.Book.find()
         .then(books => {
-            res.status(200).json(books);
+            res.json(books);
         })
         .catch(err => {
-            res.status(400).json({
-                error: err
+            // console.log(err);
+            res.status(404).json({
+                message: "Failed to fetch records"
             });
         });
 });
@@ -20,11 +21,61 @@ books.get('/', (req, res) => {
 books.get('/:id', (req, res) => {
     db.Book.findById(req.params.id)
         .then(book => {
-            res.json(book);
+            res.send(book);
         })
         .catch(err => {
-            res.status(400).json(err);
+            // console.log(err);
+            res.status(404).json({
+                message: "ObjectID not found"
+            });
         })
+});
+
+// CREATE
+books.post('/', (req, res) => {
+    db.Book.create(req.body)
+        .then(() => {
+            res.status(200).json({
+                message: "Book created successfully!"
+            })
+        })
+        .catch(err => {
+            // console.log(err)
+            res.status(400).json({
+                message: "Failed to create"
+            })
+        });
+});
+
+// UPDATE
+books.put('/:id', (req, res) => {
+    db.Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.status(200).json({
+                message: "Book updated successfully!"
+            })
+        })
+        .catch(err => {
+            // console.log(err);
+            res.status(400).json({
+                message: "Failed to update"
+            })
+        });
+});
+
+books.delete('/:id', (req, res) => {
+    db.Book.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(200).json({
+                message: "Book deleted successfully!"
+            })
+        })
+        .catch(err => {
+            // console.log(err);
+            res.status(400).json({
+                message: "Failed to delete"
+            })
+        });
 });
 
 // SEED
@@ -32,15 +83,15 @@ books.get('/seed', (req, res) => {
     db.Book.insertMany(BookSeeder)
         .then(() => {
             res.status(200).json({
-                is_success: true,
-                message: "Seed successful"
+                message: 'Seed successful'
             });
         })
         .catch(err => {
-            res.status(404).json({
-                err: err
+            // console.log(err);
+            res.status(400).json({
+                message: "Failed to seed"
             });
-        });
+        })
 });
 
 module.exports = books;
